6fc7dbb961f6a73a244b026cd06fd45d2d231aa7
Daniel130me <69328757+Daniel130me@users.noreply.github.com>
2021-12-06 18:55:18 +0100

    Delete webapp directory

--- webapp/js/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw1.js')
-    .then((reg) => console.log("sw registered", reg))
-    .catch((err) => console.log('sw not regstered', err))
-}

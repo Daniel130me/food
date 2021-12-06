52e1636a216d4d78cc5a717d2f15197f3cfaba4d
Daniel130me <69328757+Daniel130me@users.noreply.github.com>
2021-12-06 18:18:28 +0100

    Update sw.js

--- webapp/sw.js
+++ webapp/sw.js
@@ -1,6 +1,21 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    'js/ui.js',
+    '/css/style.css',
+];
 //install service worker
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed")
+//     console.log("service worker has been installed")
+    //wait until all assets are cache before install
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        }
+    )
 })
 
 //activate service worker
